eb005fe64ba7ba337f98a8c62a310255cf3093f2
Yves Vindevogel <yves@vindevogel.net>
2020-04-03 15:49:34 +0200

    Loading of pages from yaml

--- src/application/dataloader.py
+++ src/application/dataloader.py
@@ -15,10 +15,33 @@ from singleton import Singleton
 
 class DataLoader(metaclass=Singleton):
     @staticmethod
-    def get_data():
-        data_dir = os.path.join(Settings().root_dir, 'src', 'data', 'site')
-        file = open(os.path.join(data_dir, 'settings.yml'), 'r')
+    def get_settings():
+        settings_dir = os.path.join(Settings().root_dir, 'src', 'data', 'site')
+        file = open(os.path.join(settings_dir, 'settings.yml'), 'r')
 
-        data = yaml.load(file, Loader=yaml.SafeLoader)
+        settings = yaml.load(file, Loader=yaml.SafeLoader)
+
+        return settings
+
+    def get_index_data(self):
+        data = {'blog': self.get_settings()}
+
+        return data
+
+    def get_page_data(self, page):
+        data = {}
+
+        pages_dir = os.path.join(Settings().root_dir, 'src', 'data', 'pages')
+        file = open(os.path.join(pages_dir, '{0}.yml'.format(page)), 'r')
+
+        page_data = yaml.load(file, Loader=yaml.SafeLoader)
+
+        data['blog'] = self.get_settings()
+        data['page'] = page_data
+
+        return data
+
+    def get_post_data(self):
+        data = {'blog': self.get_settings()}
 
         return data
